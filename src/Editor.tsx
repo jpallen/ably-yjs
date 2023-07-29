bd1d92adb46c1a5a70cde68e8f0b20f99e232c7d
James Allen <james@jpallen.net>
2023-07-29 13:38:28 +0100

    Support cursors

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,30 +1,38 @@
 import { useEffect, useState } from "react"
+import Quill from 'quill'
 import ReactQuill from "react-quill"
 import 'react-quill/dist/quill.snow.css'
 import { QuillBinding } from "y-quill"
+import QuillCursors from 'quill-cursors'
 import * as Y from 'yjs'
+import {
+  Awareness,
+  encodeAwarenessUpdate,
+  applyAwarenessUpdate,
+  removeAwarenessStates
+} from 'y-protocols/awareness'
 import * as Ably from 'ably'
+
+Quill.register('modules/cursors', QuillCursors)
 
 const API_KEY = 'kHrEyw.Re6ZBw:xMB1rjfLpsf_IVkg4Y8gYKqK-DaaJ5blYisvXgXDb58'
 
 export const Editor = () => {
   const [quill, setQuill] = useState<ReactQuill | null>(null)
   const [doc] = useState(new Y.Doc())
+  const [awareness] = useState(new Awareness(doc))
 
   const realtime = useRealtime()
 
-  useChannel(realtime, doc)
+  useChannel(realtime, doc, awareness)
 
   useEffect(() => {
-    console.log('useEffect', doc, quill)
     if (!doc || !quill) return
-    console.log('binding')
-    doc.on('update', () => console.log('update'))
-    const binding = new QuillBinding(doc.getText('t'), quill.editor)
+    const binding = new QuillBinding(doc.getText('t'), quill.editor, awareness)
     return () => binding.destroy()
-  }, [doc, quill])
+  }, [doc, quill, awareness])
 
-  return <ReactQuill theme="snow" ref={(q) => setQuill(q)} />
+  return <ReactQuill theme="snow" ref={(q) => setQuill(q)} modules={{ cursors: true }} />
 }
 
 const useRealtime = () => {
@@ -39,25 +47,46 @@ const useRealtime = () => {
   return realtime
 }
 
-const useChannel = (realtime: Ably.Realtime | null, doc: Y.Doc) => {
+type AwarenessUpdate = {
+  added: number[]
+  removed: number[]
+  updated: number[]
+}
+
+const useChannel = (realtime: Ably.Realtime | null, doc: Y.Doc, awareness: Awareness) => {
   useEffect(() => {
     if (!realtime) return
     const channel = realtime.channels.get('docs:foo')
   
     channel.subscribe('update', (message) => {
       const update = new Uint8Array(message.data)
-      console.log('channel.subscribe: update', { update, data: message.data })
       Y.applyUpdate(doc, update)
     })
 
+    channel.subscribe('awareness', (message) => {
+      const update = new Uint8Array(message.data)
+      applyAwarenessUpdate(awareness, update, 'server')
+    })
+
     doc.on('update', (update) => {
-      console.log('doc.update', update)
       channel.publish('update', update)
+    })
+
+    awareness.on(
+      'update',
+      ({ added, updated, removed }: AwarenessUpdate, origin: any) => {
+        const changedClients = added.concat(updated).concat(removed)
+        const update = encodeAwarenessUpdate(awareness, changedClients)
+        channel.publish('awareness', update, origin)
+      }
+    )
+
+    window?.addEventListener('beforeunload', () => {
+      removeAwarenessStates(awareness, [doc.clientID], 'window unload')
     })
 
     channel.history({ direction: 'forwards', limit: 1000 }, (error, result) => {
       if (!result) return
-      console.log('history', result.items)
       for (const message of result.items) {
         const update = new Uint8Array(message.data)
         Y.applyUpdate(doc, update)
@@ -68,5 +97,5 @@ const useChannel = (realtime: Ably.Realtime | null, doc: Y.Doc) => {
       channel.detach()
       realtime.channels.release('foo')
     }
-  }, [realtime, doc])
+  }, [realtime, doc, awareness])
 }
